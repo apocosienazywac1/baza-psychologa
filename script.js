2c033f1e2b367ece30d9754ae8a12cabe5ac15ed
apocosienazywac1 <apocosienazywac1@gmail.com>
2026-04-01 12:02:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const botContainer = document.getElementById("bot-container");
+
+botContainer.innerHTML = `
+<iframe
+  src="YOUR_BOTPRESS_URL"
+  width="400"
+  height="500"
+  style="border:none;">
+</iframe>
+`;
