6fd34857d17e02549099143fdedb8defd9c57ad6
apocosienazywac1 <apocosienazywac1@gmail.com>
2026-04-01 12:37:33 +0200

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const botContainer = document.getElementById("bot-container");
 
+// Twój nowy link shareable z Botpress
 botContainer.innerHTML = `
 <iframe
   src="https://cdn.botpress.cloud/webchat/v3.6/shareable.html?configUrl=https://files.bpcontent.cloud/2026/04/01/09/20260401090115-3JZ17WG4.json"
-  id="bp-frame">
+  width="100%"
+  height="100%"
+  style="border:none;">
 </iframe>
 `;
