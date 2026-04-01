2caffbe026bcb3b93bd01fee343ebe9405183a39
apocosienazywac1 <apocosienazywac1@gmail.com>
2026-04-01 13:33:41 +0200

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const botContainer = document.getElementById("bot-container");
-
-botContainer.innerHTML = `
-<iframe
-  src="https://cdn.botpress.cloud/webchat/v3.6/shareable.html?configUrl=https://files.bpcontent.cloud/2026/04/01/09/20260401090115-3JZ17WG4.json"
-  width="100%"
-  height="100%"
-  style="border:none;">
-</iframe>
-`;
+// Inicjalizacja Webchat Botpress w kontenerze
+window.botpressWebChat.init({
+  host: 'https://cdn.botpress.cloud/webchat/v3.6',
+  botId: 'IFCO3FKO', // Twój botId
+  webchatConfig: 'https://files.bpcontent.cloud/2026/04/01/09/20260401090115-3JZ17WG4.json', // Twój link do JSON
+  botName: 'Specjalistka Agnieszka',
+  showConversationsButton: false, // usuwa ikonę w prawym dolnym rogu
+  enableReset: true,
+  enableWelcomeMessage: true,
+  container: document.getElementById('bot-container') // wstawienie bota do kontenera na stronie
+});
