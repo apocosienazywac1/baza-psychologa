b576422f46f4253ae625024d2aca76dbcc2d8c5a
apocosienazywac1 <apocosienazywac1@gmail.com>
2026-04-01 12:33:50 +0200

    script.js

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const botContainer = document.getElementById("bot-container");
 botContainer.innerHTML = `
 <iframe
   src="https://cdn.botpress.cloud/webchat/v3.6/shareable.html?configUrl=https://files.bpcontent.cloud/2026/04/01/09/20260401090115-3JZ17WG4.json"
-  width="400"
-  height="500"
-  style="border:none;">
+  id="bp-frame">
 </iframe>
 `;
